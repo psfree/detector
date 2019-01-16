468770ae1b962b11a92877d9341fa99b79e3e937
calypso <calypso.reinx@gmail.com>
2019-01-15 19:16:36 -0500

    updated code

--- detect.py
+++ detect.py
@@ -1,10 +1,13 @@
 from imutils import paths
 import numpy as np
 import cv2
+import serial
 
 def variance(image):
 	return cv2.Laplacian(image, cv2.CV_64F).var()
 
+ser1 = serial.Serial('/dev/cu.usbmodem143301', 9600) #set the correct port
+#ser1.write('f'.encode())
 
 faceCascade = cv2.CascadeClassifier('frontal.xml')
 sideCascade = cv2.CascadeClassifier('profile.xml')
@@ -22,7 +25,7 @@ while(cap.isOpened()):
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
-        minSize=(100, 100),
+        minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
     profiles = sideCascade.detectMultiScale(
@@ -36,7 +39,30 @@ while(cap.isOpened()):
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
     for (x, y, w, h) in profiles:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
-
+    
+    height, width, channels = frame.shape
+    if(len(faces)==0):
+    	ser1.write('c'.encode())
+    	print("c")
+    fudge = 50
+    for (x,y,w,h) in faces:
+        pos1  = x+w/2
+        pos2  = y+h/2
+        s1 = 'c'
+        s2 = 'c'
+        if(pos2 < height/2 -fudge):
+             s1 = 'u'
+        elif(pos2 > height/2 + fudge):
+             s1 = 'd'
+             
+        if(pos1 < width/2 -fudge):
+             s2 = 'r'
+        elif(pos1 > width/2 + fudge):
+             s2= 'l'
+        s= s1+s2 +'\n';
+        ser1.write(s.encode())
+        print(s.encode())
+        break
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
